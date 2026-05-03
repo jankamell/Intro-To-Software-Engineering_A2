151e55128fd24c56714cefd9f558aeaac3339bfb
mazenn693 <mazensheriff888@gmail.com>
2026-05-03 19:14:20 +0200

    usermodel &budget services and controler

---
+++ srccc/src/main/java/controllers/BudgetController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import services.*;
+import models.*;
+
+public class BudgetController {
+
+    private BudgetService service = new BudgetService();
+
+
+    public void setBudget(User u, double b) {
+        service.createBudget(u, b);
+    }
+
+    public double getSpent(User u) {
+        return service.calculateSpent(u);
+    }
+
+    
+    public void check(User u) {
+        service.checkBudget(u);
+    }
+}
