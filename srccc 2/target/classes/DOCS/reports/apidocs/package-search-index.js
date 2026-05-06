97e187e1408e1d11b6c93bf6d1bb1ea99dcc311e
jankamell <jankamel777@gmail.com>
2026-05-06 14:19:50 +0300

    Ignore build artifacts and system files

--- srccc 2/target/classes/DOCS/reports/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"app"},{"l":"controllers"},{"l":"models"},{"l":"services"},{"l":"storage"},{"l":"ui"}];updateSearchResults();
