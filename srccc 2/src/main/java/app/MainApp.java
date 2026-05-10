e21a1aaf13147b9f39e0e79bb6cd730d409ee7fb
jankamell <jankamel777@gmail.com>
2026-05-10 13:54:57 +0300

    fixing

--- srccc 2/src/main/java/app/MainApp.java
+++ srccc 2/src/main/java/app/MainApp.java
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
-=======
 //mvn clean javafx:run
->>>>>>> 705c469 (Restoring project files from srccc 2)
-package app;
 
+package app;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import ui.LoginView;
 import storage.Storage;
 
-<<<<<<< HEAD
-public class MainApp extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        Storage.load();
-=======
 /**
  * The main entry point for the Budget Management Application.
  * This class initializes the application storage and sets up the primary 
@@ -34,19 +24,15 @@ public class MainApp extends Application {
         // Loads existing data from memory or disk
         Storage.load(); 
         
->>>>>>> 705c469 (Restoring project files from srccc 2)
         stage.setTitle("Budget App");
         stage.setScene(new Scene(new LoginView(stage), 600, 450));
         stage.show();
     }
 
-<<<<<<< HEAD
-=======
     /**
      * The standard main method that launches the JavaFX application.
      * @param args Command line arguments.
      */
->>>>>>> 705c469 (Restoring project files from srccc 2)
     public static void main(String[] args) {
         launch();
     }
